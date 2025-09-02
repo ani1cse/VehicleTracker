39ee2ed298c1c80f32f272feae4b3fff7336d5d2
ani1cse <ani1cse@outlook.com>
2025-09-02 10:27:38 +0600

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,30 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Vehicle Log Manager',
-        short_name: 'Vehilog',
-        theme_color: '#0d9488',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
-})
+  plugins: [react()],
+  base: '/VehicleTracker/'  
+});
