ac033825425651d1311f726afedb23c0400b6ce5
ani1cse <ani1cse@outlook.com>
2025-09-09 11:05:41 +0600

    Update jsonbin.js

--- src/api/jsonbin.js
+++ src/api/jsonbin.js
@@ -1,6 +1,26 @@
 const BIN_ID = "68b3294a43b1c97be930f889";
 const API_KEY = "$2a$10$g1kqJpzAdMvBv8./6ZwR7Od3ohSGRq6Db298bV.rCDwkohvSfPNrG"; 
 
+export const saveVehicles = async (vehicles) => {
+  const res = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+    headers: { "X-Master-Key": API_KEY },
+  });
+  const data = await res.json();
+
+  await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key": API_KEY,
+      "X-Bin-Versioning": "false",
+    },
+    body: JSON.stringify({
+      ...data.record,
+      vehicles,
+    }),
+  });
+};
+
 export const fetchVehicles = async () => {
   const res = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
     headers: { "X-Master-Key": API_KEY },
